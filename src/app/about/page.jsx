994f21d8fd4a5f09969383dae06c6a7425f4a77b
Sametgncc <114944484+Sametgncc@users.noreply.github.com>
2024-10-13 18:31:20 +0300

    routing

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Page = () => {
   return (
     <div>
       About page
+      <p>
+        <Link href="/">Home</Link>
+      </p>
+      <p>
+        <Link
+          href={{
+            pathname: "/products",
+            query: {
+              p: "John",
+              page: 1,
+              limit: 10,
+              sort: "asc",
+              order: "desc",
+              status: "active",
+              category: "laptop",
+            },
+          }}
+        />
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
